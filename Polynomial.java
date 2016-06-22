8f312db72c5564341b4a987aaef1a7952431e443
selim3 <selim.lushtaku@hotmail.com>
2016-06-22 17:21:44 +0200

    Add files via upload

---
+++ Polynomial.java
@@ -0,0 +1,24 @@
+public class Polynomial {
+	
+	private double[] terms;
+	
+	public Polynomial(double[] t){
+		terms = t;
+	}
+	
+	public double[] toArray(){
+		return terms;
+	}
+		
+	public double evaluate(double x){
+		
+		double result = terms[terms.length-1];
+		
+		for(int i=terms.length-2; i>=0; i--){
+			result = (result*x)+terms[i];
+		}
+		
+		return result;
+	}
+
+}
